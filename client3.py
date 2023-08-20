7bee7ddc85402c8f3761e67a37d5a87d2c66af9b
Arunpandiyan-12 <arunpandiyans.aids2020@citchennai.net>
2023-08-20 14:07:45 +0530

    updated the client3  file

--- client3.py
+++ client3.py
@@ -28,32 +28,44 @@ QUERY = "http://localhost:8080/query?id={}"
 # 500 server request
 N = 500
 
-
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2  # Calculate price correctly
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    # Calculate the ratio of price_a to price_b
+    if price_b != 0:
+        ratio = price_a / price_b
+    else:
+        ratio = 0  # To handle division by zero
+    return ratio
 
-
+# Main
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        prices = {}  # Dictionary to store stock prices
+
+        for quote in quotes:
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price  # Store price in dictionary
+
+        # Calculate ratio using prices from dictionary
+        ratio = getRatio(prices['ABC'], prices['DEF'])
+
+        # Print the stock quotes and calculated ratio
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % ratio)
+
--- client_test.py
+++ client_test.py
@@ -2,24 +2,37 @@ import unittest
 from client3 import getDataPoint
 
 class ClientTest(unittest.TestCase):
-  def test_getDataPoint_calculatePrice(self):
-    quotes = [
-      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+    def test_getDataPoint_calculatePrice(self):
+        quotes = [
+            {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+        # Calculate expected results based on the formula (bid_price + ask_price) / 2
+        expected_results = [
+            ('ABC', 120.48, 121.2, 120.84),
+            ('DEF', 117.87, 121.68, 119.775)
+        ]
 
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
-    quotes = [
-      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+        for i, quote in enumerate(quotes):
+            with self.subTest(quote=quote):
+                result = getDataPoint(quote)
+                self.assertEqual(result, expected_results[i])
 
+    def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+        quotes = [
+            {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+        # Calculate expected results based on the formula (bid_price + ask_price) / 2
+        expected_results = [
+            ('ABC', 120.48, 119.2, 119.84),
+            ('DEF', 117.87, 121.68, 119.775)
+        ]
 
-  """ ------------ Add more unit tests ------------ """
-
-
+        for i, quote in enumerate(quotes):
+            with self.subTest(quote=quote):
+                result = getDataPoint(quote)
+                self.assertEqual(result, expected_results[i])
 
 if __name__ == '__main__':
     unittest.main()
